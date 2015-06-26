6029537d97b75435a6f63e7a891dc1925eb1b4de
matthewvitt <matthewvitt@gmail.com>
2015-06-26 10:32:14 -0700

    files deleted

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,17 +8,22 @@ class GamesController < ApplicationController
 
 	def save
         user = User.find(params[:user_id].to_i)
-        # user.scores << Score.create({
-        #   :level1 => params[:level1],
-        #   :level2 => params[:level2],
-        #   :level3 => params[:level3],
-        #   :total => params[:total],   
-        #   :user => @user.name
-        # })
-        # user.save'
+        user.scores << Score.create({
+          :level1 => params[:level1],
+          :level2 => params[:level2],
+          :level3 => params[:level3],
+          :total => params[:total],   
+          :user => params[@user_params]
+        })
+        user.save
      respond_to do |format|
            format.json { render :json => {:message => "Success"} }
+        end
     end
-  end
+
   
+  private
+    def user_params
+        params.require(:user).permit(:name)
+    end
 end
